d01d6cf58b4501dc8b2d69657ef98f9c8cde15f5
Lulu_User1 <fortniteacccreation1@gmail.com>
2025-04-15 22:20:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const searchBar = document.getElementById("searchBar");
     const onionSwitch = document.getElementById("onionSwitch");
 
     // Default state OFF
     onionSwitch.checked = false;
+
+    // Redirect to DuckDuckGo on Enter key press
+    searchBar.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            window.location.href = "https://duckduckgo.com/";
+        }
+    });
 });
